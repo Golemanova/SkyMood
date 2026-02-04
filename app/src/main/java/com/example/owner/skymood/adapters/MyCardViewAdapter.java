fcab8c54f0a4792738fdc7ac154b150f008a3b0d
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2026-02-04 10:55:48 +0200

    Update MyCardViewAdapter.java

--- app/src/main/java/com/example/owner/skymood/adapters/MyCardViewAdapter.java
+++ app/src/main/java/com/example/owner/skymood/adapters/MyCardViewAdapter.java
@@ -1,7 +1,6 @@
 package com.example.owner.skymood.adapters;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +8,8 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.owner.skymood.R;
 import com.example.owner.skymood.model.LocationPreference;
