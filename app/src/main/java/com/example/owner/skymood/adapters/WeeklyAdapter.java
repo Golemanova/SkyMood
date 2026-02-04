ed51ac2c27b65d5eedcf5dfebf7070001de29db7
Ivelina Golemanova <ivelinagolemanova@gmail.com>
2026-02-04 10:56:32 +0200

    Update WeeklyAdapter.java

--- app/src/main/java/com/example/owner/skymood/adapters/WeeklyAdapter.java
+++ app/src/main/java/com/example/owner/skymood/adapters/WeeklyAdapter.java
@@ -1,10 +1,13 @@
 package com.example.owner.skymood.adapters;
 
+package com.example.owner.skymood.adapters;
+
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.owner.skymood.R;
 import com.example.owner.skymood.model.WeeklyWeather;
